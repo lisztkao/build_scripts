3fdc08ac55bac2ecfd75a64ab3e4973cb8a35397
liszt.kao <liszt.kao@advantech.com.tw>
2019-11-21 13:22:34 +0800

    [Andorid9]Add rom5720/rom7720 config.

--- imx6/android/all_imx6_android_P9_dailybuild.sh
+++ imx6/android/all_imx6_android_P9_dailybuild.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 MACHINE_LIST=""
 
-# Check build machine name
-#if [ $RSB4410A1 == true ]; then
-#        MACHINE_LIST="$MACHINE_LIST rsb_4410_a1"
-#fi
 if [ $RSB4411A1 == true ]; then
 	MACHINE_LIST="$MACHINE_LIST rsb_4411_a1"
+fi
+if  [ $ROM5720A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom5720_a1"
+fi
+if  [ $ROM7720A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom7720_a1"
 fi
 
 export MACHINE_LIST
