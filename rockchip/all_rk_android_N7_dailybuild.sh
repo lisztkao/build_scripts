853bbc31fd7c43cef22a984011fac44e7a2fbd89
Liszt.Kao <liszt.kao@advantech.com.tw>
2021-07-03 15:01:53 +0800

    Add ds211 MP config.

--- rockchip/all_rk_android_N7_dailybuild.sh
+++ rockchip/all_rk_android_N7_dailybuild.sh
@@ -43,6 +43,16 @@ export KERNEL_CONFIG
 export MACHINE_LIST
 ./rk_android_N7_dailybuild.sh ds211 $VERSION_NUM
 fi
+#DS211_MP_projects
+if [ "$DS211_MP" == "true" ]; then
+        KERNEL_DTB=rk3399-ds211.img
+        KERNEL_CONFIG=rockchip_ds211_defconfig
+        MACHINE_LIST="$MACHINE_LIST ds211"
+export KERNEL_DTB
+export KERNEL_CONFIG
+export MACHINE_LIST
+./rk_android_N7_dailybuild.sh ds211_MP $VERSION_NUM
+fi
 #DS100_DeviceOn_projects
 if [ "$DS100_DeviceOn" == "true" ]; then
 	KERNEL_DTB=rk3399-ds100.img
