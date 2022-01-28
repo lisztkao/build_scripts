c25d18488548bfc4e6c9ad4f0b71ec97d5600dcd
Liszt.Kao <liszt.kao@advantech.com.tw>
2022-01-28 17:16:12 +0800

    add azbuild.

--- nvidia/azure_build.sh
+++ nvidia/azure_build.sh
@@ -1,59 +1,31 @@
-#!/bin/bash -ex
-sudo apt-get update
-sudo apt-get install --reinstall kmod
+#!/bin/bash
+TOPDIR=`pwd`
+DOCKER_IMAGE="advrisc/u18.04-imx8lbv1:latest"
 
-#git config --global user.name lisztkao
-#git config --global user.email lisztru@gmail.com
-#git clone https://github.com/lisztkao/build_scripts.git
+while [ $# -gt 0 ]; do
+	case ${1} in
+		-air020)
+			SOC="${2}"
+			shift 2
+			;;
+		-VERSION)
+			VERSION="${2}"
+			shift 2
+			;;
+		*)
+			echo "Error: Invalid option ${1}"
+			exit 1
+			;;
+		esac
+done
 
-export MACHINE_LIST="$MACHINE_LIST"
-export BUILD_NUMBER="$BUILD_NUMBER"
-export DATE=`date +%F`
-export STORED="./stored"
-mkdir $STORED
+echo "VERSION:$VERSION"
+echo "air020:$air020"
 
-export FTP_SITE="172.22.15.143"
-export FTP_DIR="RISC-Nvidia-Ubuntu18"
-
-#--- [platform specific] ---
-
-# export BSP_URL="https://gitlab.wise-paas.com/air-020/manifests.git"
-export BSP_URL="https://AIM-Linux@dev.azure.com/AIM-Linux/AIR-020/_git/manifests"
-export BSP_BRANCH="master"
-export BSP_XML="default.xml"
-export RELEASE_VERSION="V0001"
-export BUILDALL_DIR="build_all"
-
-export PATH="/home/adv/bin:${PATH}"
-#mv build_scripts/nvidia/* .
-
-
-set +e
-./all_nv_ubuntu_dailybuild.sh
-rc="$?"
-set -e
-#---------------------------
-cd $STORED/$DATE
-pftp -v -n ${FTP_SITE} <<-EOF
-  user "essci\\user" "P@ssw0rdQA"
-  cd "Product\\${FTP_DIR}\\Daily Build"
-  mkdir $DATE
-  cd $DATE
-  prompt
-  binary
-  mput *
-  close
-  quit
-EOF
-
-sudo rm * -rf
-#---[return code]------------
-if [ "$rc" == "0" ]; then
-  exit 0;
-else
-  exit 1;
-fi
-
+export DOCK_ID=$(sudo docker run -it --name jetson_linux_risc -v $TOPDIR:/home/adv/BSP:rw --privileged $DOCKER_IMAGE /bin/bash)
+sudo docker exec $DOCKER_ID /bin/bash -c "./azure_docker_build.sh"
+sudo docker stop $DOCK_ID
+sudo docker rm $DOCK_ID
 
 
 	
