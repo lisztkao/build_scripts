b94d301765df80f060bbffbafec5475d7cb25249
Liszt.Kao <liszt.kao@advantech.com.tw>
2022-02-07 10:46:11 +0800

    add

--- nvidia/azure_build.sh
+++ nvidia/azure_build.sh
@@ -23,7 +23,7 @@ echo "VERSION:$VERSION"
 echo "air020:$air020"
 echo "sudo docker pull $DOCKER_IMAGE"
 sudo docker pull $DOCKER_IMAGE
-export DOCK_ID=$(sudo docker run -it --name jetson_linux_risc -v $TOPDIR:/home/adv/BSP:rw --privileged $DOCKER_IMAGE /bin/bash)
+export DOCK_ID=$(sudo docker run --name jetson_linux_risc -v $TOPDIR:/home/adv/BSP:rw --privileged $DOCKER_IMAGE /bin/bash)
 echo "sudo docker exec $DOCKER_ID /bin/bash -c ./azure_docker_build.sh"
 sudo docker exec $DOCKER_ID /bin/bash -c "./azure_docker_build.sh"
 sudo docker stop $DOCK_ID
