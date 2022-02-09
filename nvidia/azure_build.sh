258afb7768558dc1bee702bb7e0d3995fb461f99
Liszt.Kao <liszt.kao@advantech.com.tw>
2022-02-09 16:55:47 +0800

    add

--- nvidia/azure_build.sh
+++ nvidia/azure_build.sh
@@ -3,7 +3,8 @@ TOPDIR=`pwd`
 DOCKER_IMAGE="advrisc/u18.04-imx8lbv1:latest"
 CONTAINER_NAME="jetson_linux_risc"
 WORK_DIR="/ext/docker/nvidia/"
-GIT_BUILD_SCRIPT="https://github.com/lisztkao/build_scripts.git"
+GIT_BUILD_FOLDER="build_scripts"
+GIT_BUILD_SCRIPT="https://github.com/lisztkao/${GIT_BUILD_FOLDER}.git"
 SAS_KEY="?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-02-09T15:40:31Z&st=2022-02-09T07:40:31Z&spr=https&sig=nxS6KIrgyzsGSKWA6tWUgGI658i18uLWqDBKpTLF4MQ%3D"
 STORAGE_ACCOUNT="riscsw"
 BLOB_CONTAINER="image"
@@ -53,9 +54,10 @@ if [ ! -z "$status" ]; then
 	exit 1
 fi
 
-sudo git clone $GIT_BUILD_SCRIPT $WORK_DIR
+sudo git clone $GIT_BUILD_SCRIPT $WORK_DIR/${GIT_BUILD_FOLDER}/
 docker exec $CONTAINER_NAME /bin/bash -c "sudo chown adv:adv -R BSP"
-docker exec $CONTAINER_NAME /bin/bash -c "cd BSP/;cp nvidia/${BUILD_IMAGE_SCRIPT} .;ls -a;source ./${BUILD_IMAGE_SCRIPT} -p $PRODUCT -s $SOC -v $VERSION -d $DEVICEON"
+docker exec $CONTAINER_NAME /bin/bash -c "cd BSP/;cp ${GIT_BUILD_FOLDER}/nvidia/${BUILD_IMAGE_SCRIPT} .;ls -a"
+docker exec $CONTAINER_NAME /bin/bash -c "cd BSP/;source ./${BUILD_IMAGE_SCRIPT} -p $PRODUCT -s $SOC -v $VERSION -d $DEVICEON"
 azcopy cp $WORK_DIR/$OUTPUT_DIR $AZCOPY_URL
 #docker exec $CONTAINER_NAME /bin/bash -c "cd BSP/;cp nvidia/${BUILD_IMAGE_SCRIPT} .;ls -a;"
 #azcopy cp $WORK_DIR/$OUTPUT_DIR $AZCOPY_URL --recursive
