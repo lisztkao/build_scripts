4955ffe95c98a197127e064f6dde1956e098a899
Liszt.Kao <liszt.kao@advantech.com.tw>
2022-03-09 09:44:09 +0800

    add

--- nvidia/all_nv_ubuntu_dailybuild.sh
+++ nvidia/all_nv_ubuntu_dailybuild.sh
@@ -6,18 +6,18 @@ echo "air020:$air020"
 if [ "$air020" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST air020"
 	export MACHINE_LIST
-	./nv_ubuntu_dailybuild.sh air020 "${VERSION}"
+	./nv_ubuntu_dailybuild.sh air020 "${VERSION}" "${SOC}"
 fi
 
 if [ "$air020_deviceon" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST air020"
 	export MACHINE_LIST
-	./nv_ubuntu_dailybuild_deviceon.sh air020 "${VERSION}"
+	./nv_ubuntu_dailybuild_deviceon.sh air020 "${VERSION}" "${SOC}"
 fi
 
 #EPC-R7200_projects
 if [ "$epcr7200" == "true" ]; then
-	MACHINE_LIST="$MACHINE_LIST epcr7200"
+	MACHINE_LIST="$MACHINE_LIST epcr7200" 
 	export MACHINE_LIST
 	./nv_ubuntu_dailybuild.sh epcr7200 "${VERSION}" "${SOC}"
 fi
