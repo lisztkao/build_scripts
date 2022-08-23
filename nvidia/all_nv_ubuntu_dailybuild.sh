a196b04feebd300087b0c062ba69c5e215d1e544
Liszt.Kao <liszt.kao@advantech.com.tw>
2022-08-23 17:32:04 +0800

    add

--- nvidia/all_nv_ubuntu_dailybuild.sh
+++ nvidia/all_nv_ubuntu_dailybuild.sh
@@ -6,26 +6,26 @@ echo "air020:$air020"
 if [ "$air020" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST air020"
 	export MACHINE_LIST
-	./nv_ubuntu_dailybuild.sh air020 "${SOC}" "${VERSION}"
+	./nv_ubuntu_dailybuild.sh air020 "${SOC}" "${VERSION}" "${VER_PREFIX}"
 fi
 
 if [ "$air020_deviceon" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST air020"
 	export MACHINE_LIST
-	./nv_ubuntu_dailybuild_deviceon.sh air020 "${SOC}" "${VERSION}"
+	./nv_ubuntu_dailybuild_deviceon.sh air020 "${SOC}" "${VERSION}" "${VER_PREFIX}"
 fi
 
 #EPC-R7200_projects
 if [ "$epcr7200" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST epcr7200" 
 	export MACHINE_LIST
-	./nv_ubuntu_dailybuild.sh epcr7200 "${SOC}" "${VERSION}"
+	./nv_ubuntu_dailybuild.sh epcr7200 "${SOC}" "${VERSION}" "${VER_PREFIX}"
 fi
 
 if [ "$epcr7200_deviceon" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST epcr7200"
 	export MACHINE_LIST
-	./nv_ubuntu_dailybuild_deviceon.sh epcr7200 "${SOC}" "${VERSION}"
+	./nv_ubuntu_dailybuild_deviceon.sh epcr7200 "${SOC}" "${VERSION}" "${VER_PREFIX}"
 fi
 
 echo "[ADV] All done!"
--- nvidia/all_nv_ubuntu_officialbuild.sh
+++ nvidia/all_nv_ubuntu_officialbuild.sh
@@ -4,14 +4,14 @@ MACHINE_LIST=""
 if [ "$air020" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST air020"
 	export MACHINE_LIST
-	./nv_ubuntu_officialbuild.sh air020 "${VERSION}" "${SOC}"
+	./nv_ubuntu_officialbuild.sh air020 "${VERSION}" "${SOC}" "${VER_PREFIX}"
 fi
 
 #EPC-R7200_projects
 if [ "$epcr7200" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST epcr7200"
 	export MACHINE_LIST
-	./nv_ubuntu_officialbuild.sh epcr7200 "${VERSION}" "${SOC}"
+	./nv_ubuntu_officialbuild.sh epcr7200 "${VERSION}" "${SOC}" "${VER_PREFIX}"
 fi
 echo "[ADV] All done!"
 
