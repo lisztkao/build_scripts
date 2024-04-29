c148b3718724d49ae9432425fe0f4c38bb7e6414
liszt.kao <liszt.kao@advantech.com.tw>
2024-04-29 11:08:08 +0800

    Modify qcs aim37 for afed260.

--- qcs/all_risc_qcs_linux_dailybuild_aim37.sh
+++ qcs/all_risc_qcs_linux_dailybuild_aim37.sh
@@ -9,4 +9,13 @@ if [ "$ROM2860A1" == "true" ]; then
     ./risc_qcs_linux_dailybuild_aim37.sh ROM2860A1
 fi
 
+#AFED260A1 Project
+if [ "$AFED260A1" == "true" ]; then
+    MODEL_NAME="8550"
+    BOARD_VER="A1"
+    export MODEL_NAME
+    export BOARD_VER
+    ./risc_qcs_linux_dailybuild_aim37.sh ROM2860A1
+fi
+
 echo "[ADV] All done!"
